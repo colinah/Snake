26e5b6831a42c2b4060fcd80a0c6a6af872f179e
Colin Hale <halecolin1@gmail.com>
2019-11-21 13:51:01 -0700

    Added movement to snake

--- Snake.js
+++ Snake.js
@@ -1,10 +1,54 @@
 function Snake() {
     this.scale = 10;
-    this.x = 1;
-    this.y =1;
+    this.body = [{x:1,y:1}];
+    this.head = {x:1,y:1};
+    this.move = {x:0,y:1}
+
+    this.draw = (direction) => {
+        this.show()
+        this.movement()
+        this.changeDirection(direction)
+    }
 
     this.show = () => {
-        rect(this.x,this.y,this.scale,this.scale)
         fill(0)
+        rect(this.head.x, this.head.y, this.scale, this.scale);
+        // for( let i = 0; i < this.body.length; i++ ){
+        //     rect(this.body[i].x, this.body[i].y, this.scale, this.scale)
+        // }
+        
+    }
+
+    this.movement = () => {
+        this.head.x += this.scale*this.move.x;
+        this.head.y += this.scale*this.move.y;
+    }
+
+    this.changeDirection = (direction) => {
+        if(direction === 'right'){
+            if(this.move.x === -1){
+                return
+            } else {
+            this.move = {x:1,y:0};
+            }
+        } else if(direction === 'left') {
+            if(this.move.x === 1){
+                return
+            } else {
+            this.move = {x:-1,y:0}
+            }
+        } else if(direction === 'up') {
+            if(this.move.y === 1){
+                return
+            } else {
+                this.move = {x:0,y:-1}
+            }
+        } else if(direction === 'down') {
+            if(this.move.y === -1){
+                return
+            } else {
+                this.move = {x:0,y:1}
+            }
+        }
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,16 +1,27 @@
 let snake = new Snake()
-
-
+let direction = {x:1,y:0}
 function setup() {
   
   createCanvas(400,400)
-  
+  frameRate(10);
   console.log(snake)
 }
 
 function draw() {
   // put drawing code here
   background(220)
-  snake.show()
+  snake.draw(direction)
 
+}
+
+function keyPressed() {
+  if(keyCode === LEFT_ARROW) {
+    direction = 'left'
+  } else if(keyCode === RIGHT_ARROW) {
+    direction = 'right'
+  } else if(keyCode === UP_ARROW) {
+    direction = 'up'
+  } else if(keyCode === DOWN_ARROW) {
+    direction = 'down'
+  }
 }
