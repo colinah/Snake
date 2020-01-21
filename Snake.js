660d90ebe63038e8c683932cf6179e11eda7d2f8
PaulM-Robertson <paulmartin.robertson@gmail.com>
2020-01-21 12:16:13 -0700

    did all kinds of cool shit with snakes

--- Snake.js
+++ Snake.js
@@ -1,16 +1,19 @@
 function Snake() {
     this.scale = 10;
-    this.body = [{x:1,y:1}];
+    this.body = [];
     this.head = {x:0,y:0};
     this.move = {x:0,y:1};
     this.food = {x:-15,y:-15};
 
-    this.draw = (direction) => {
-        this.show()
-        this.movement()
-        this.changeDirection(direction)
-        this.eatFood();
-        console.log('food:', this.food, ' head:', this.head)
+    this.draw = (direction) => { 
+        this.show(); // no logic
+        if(this.body.length > 0) this.segmentTrail(); 
+
+        this.eatFood()
+        this.movement();  
+        this.death();    
+        this.changeDirection(direction);
+        // console.log('food:', this.food, ' head:', this.head)
     }
 
     
@@ -18,10 +21,33 @@ function Snake() {
         fill(0)
         rect(this.head.x, this.head.y, this.scale, this.scale);
         rect(this.food.x, this.food.y, this.scale, this.scale);
-        // for( let i = 0; i < this.body.length; i++ ){
-        //     rect(this.body[i].x, this.body[i].y, this.scale, this.scale)
-        // }
-        
+        console.log(this.body)
+        this.body.forEach( (element) => {
+            rect(element.x,element.y, this.scale, this.scale);
+        }) 
+    }
+
+    this.death = () => {
+        let isDead = false;
+        for (let i = 1; i < this.body.length; i++) {
+            if (this.body[i].x === this.head.x || this.body[i].y === this.head.y) isDead = true;
+        };
+        if (this.head.y < 0 || this.head.y >= 400 || this.head.x < 0 || this.head.x >= 400 || isDead) {
+            alert("You Dead. AWWW shit");
+            this.reset();
+        }
+
+        //death alert => if death, then alert message
+        //if snake crosses boundary line result is death
+
+    }
+    this.reset = () => {
+        this.scale = 10;
+        this.body = [];
+        this.head = {x:0,y:0};
+        this.move = {x:0,y:1};
+        this.food = {x:-15,y:-15};
+        this.generateFood();
     }
 
     this.movement = () => {
@@ -32,12 +58,25 @@ function Snake() {
     this.generateFood = () => {
         this.food.x = Math.floor(Math.random()*40)*10;
         this.food.y = Math.floor(Math.random()*40)*10;
+        if (this.food.x === 0 || this.food.x === 390 || this.food.y === 0 || this.food.y === 390) {
+            this.generateFood();
+        }
     }
 
     this.eatFood = () => {
         if(this.head.x === this.food.x && this.head.y === this.food.y) {
             this.generateFood();
+            this.foodSynthesis();
         }
+    }
+
+    this.foodSynthesis = () => {
+        this.body.push(Object.assign({},this.head));
+    }
+
+    this.segmentTrail = () => {
+        this.body.unshift(Object.assign({},this.head));
+        this.body.pop();
     }
 
     this.changeDirection = (direction) => {
